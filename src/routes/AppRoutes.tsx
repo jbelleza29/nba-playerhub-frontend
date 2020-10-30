9a73abe640d2b958a661e19099f401d0cc9b5ad5
Pritz Belleza <pritz.belleza@gmail.com>
2020-10-30 16:03:18 +0800

    add players list to routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,12 +4,16 @@ import { Switch, Route } from 'react-router-dom'
 const LandingPage = lazy(async () =>
   import('scenes/LandingPage/LandingPage'),
 );
+const PlayersList = lazy(async () =>
+  import('scenes/Players/PlayersList'),
+);
 
 export default function AppRoutes(): ReactElement {
   return (
     <Suspense fallback={<div>Loading..</div>}>
       <Switch>
         <Route path='/' exact component={LandingPage} />
+        <Route path='/players' exact component={PlayersList} />
       </Switch>
     </Suspense>
   )

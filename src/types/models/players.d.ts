1ef14500eeb4863beeba5cecf85a7a2dc3416269
Pritz Belleza <pritz.belleza@gmail.com>
2020-11-02 16:06:33 +0800

    add ts models for teams and players

---
+++ src/types/models/players.d.ts
@@ -0,0 +1,10 @@
+interface Player {
+  id: number;
+  height_feet?: number;
+  height_inches?: number;
+  first_name: string;
+  last_name: string;
+  position: string;
+  weight_pounds?: number;
+  team: Team;
+}
---
+++ src/types/models/team.d.ts
@@ -0,0 +1,7 @@
+interface Team {
+  abbrevation: string;
+  conference: string;
+  division: string;
+  full_name: string;
+  name: string;
+}

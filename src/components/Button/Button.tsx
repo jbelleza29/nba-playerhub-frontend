f927e2427c6f59977242c9ee304572871ffed533
Pritz Belleza <pritz.belleza@gmail.com>
2020-11-02 16:05:10 +0800

    add more props to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,46 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, SyntheticEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Button } from 'antd';
 
-interface ButtonProps {
-  text: string;
-  onClick?: (e: any) => void;
+export interface ButtonProps {
+  type?: 'primary' | 'text' | 'link' | 'ghost' | 'default' | 'dashed';
+  shape?: 'circle' | 'round';
+  text?: string;
+  onClick?: (e: SyntheticEvent) => void;
   link?: Boolean;
   linkPath?: any;
   value?: any;
+  icon?: React.ReactNode;
+  danger?: boolean;
 }
 
-export default function CustomButton({ text, onClick, link, linkPath, value }: ButtonProps): ReactElement {
-  console.log(linkPath, link)
+export default function CustomButton({ 
+  text, 
+  onClick, 
+  link, 
+  linkPath, 
+  value, 
+  type, 
+  shape,
+  icon,
+  danger
+ }: ButtonProps): ReactElement {
+
   return (
     <div>
       { link ? 
         <Link to={linkPath}>{text}</Link> : 
-        <Button type='primary' onClick={onClick} value={value}>{text}</Button> 
+        <Button 
+          type={type}
+          onClick={onClick}
+          shape={shape}
+          value={value}
+          icon={icon}
+          danger={danger}
+        >
+          {text}
+        </Button> 
       }
     </div>
   )

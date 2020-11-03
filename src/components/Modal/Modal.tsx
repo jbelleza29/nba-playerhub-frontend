f992689fa330f0cc3aa7a2e18464d9548db90bf5
Pritz Belleza <pritz.belleza@gmail.com>
2020-11-03 21:21:27 +0800

    add initial modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,41 @@
+import React, { ReactElement } from 'react';
+import Modal from 'antd/lib/modal/Modal';
+
+export interface ModalProps {
+  visible: boolean;
+  children?: React.ReactNode;
+  onCancel?: () => void;
+  onOk?: () => void;
+  title?: string;
+  cancelText?: string | React.ReactNode;
+  okText?: string | React.ReactNode;
+  confirmLoading?: boolean;
+  footer?: React.ReactNode;
+}
+
+export default function ModalBase({ 
+  visible, 
+  children,
+  onCancel,
+  onOk,
+  title,
+  cancelText,
+  okText,
+  confirmLoading,
+  footer
+}: ModalProps): ReactElement {
+  return (
+    <Modal 
+      visible={visible}
+      onCancel={onCancel}
+      onOk={onOk}
+      title={title}
+      cancelText={cancelText}
+      okText={okText}
+      confirmLoading={confirmLoading}
+      footer={footer}
+    >
+      {children}
+    </Modal>
+  )
+}

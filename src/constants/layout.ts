b6d3c4ca57004c1fd50ad9a7214b29382145febe
Pritz Belleza <pritz.belleza@gmail.com>
2020-11-10 03:05:11 +0800

    add navigation to menu

--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -1,6 +1,7 @@
 export const menuItems = [
   {
     key: 'players',
-    label: 'Players'
+    label: 'Players',
+    path: '/players'
   }
 ]
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 
 import { menuItems } from 'constants/layout';
@@ -10,11 +11,22 @@ interface MainLayoutProps {
 
 export default function MainLayout({ children }: MainLayoutProps): ReactElement {
   const { Header, Content } = Layout;
+  const history = useHistory();
+
+  const onNavigateMenu = (...args: any): void => {
+    if(args[0].key){
+      history.push(args[0].key);
+    }
+  }
+  const onNavigateHome = (): void => {
+    history.push('/');
+  }
+
   return (
     <Layout className='main-layout'>
       <Header>
-        <div className='logo' />
-        <Menu theme='dark' mode='horizontal'>
+        <div className='logo' onClick={onNavigateHome}/>
+        <Menu theme='dark' mode='horizontal' onClick={onNavigateMenu}>
           {menuItems.map((item) => <Menu.Item key={item.key}>{item.label}</Menu.Item>)}
         </Menu>
       </Header>

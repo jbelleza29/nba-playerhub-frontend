788bbbd51913a52966d11266d88d0f67e8e7cadc
Pritz Belleza <pritz.belleza@gmail.com>
2020-11-10 22:29:04 +0800

    add design changes

--- src/scenes/LandingPage/LandingPage.tsx
+++ src/scenes/LandingPage/LandingPage.tsx
@@ -3,12 +3,13 @@ import { useHistory } from 'react-router-dom';
 import { Carousel } from 'antd';
 
 import MainLayout from 'layouts/MainLayout';
-import TextImage from 'components/Image/TextImage';
 import Button from 'components/Button/Button';
 
 import carousel1 from 'assets/images/carousel-1.jpg';
 import carousel2 from 'assets/images/carousel-2.jpg';
 import carousel3 from 'assets/images/carousel-3.jpg';
+
+import './landing-page.scss';
 
 export default function LandingPage(): ReactElement {
   const history = useHistory();
@@ -18,19 +19,26 @@ export default function LandingPage(): ReactElement {
 
   return (
     <MainLayout>
-      <Carousel 
-        autoplay
-        autoplaySpeed={5000}
-      >
-        {[carousel3, carousel2, carousel1].map((item, index) => 
-          <TextImage 
-            key={`carousel-item-${index}`}
-            src={item}
-            text="See Player's Stats"
-            button={<Button text='View Players' type='primary' onClick={onNavigate} />}
-          />
-        )}
-      </Carousel>
+      <div className='landing-page'>
+        <Carousel 
+          autoplay
+          autoplaySpeed={5000}
+          className='carousel'
+        >
+          {[carousel3, carousel2, carousel1].map((item, index) => 
+            <img
+              src={item} 
+              alt={`carousel-${index}`}
+              key={`carousel-${index}`}
+              className='image'
+            />
+          )}
+        </Carousel>
+        <div className='text'>
+          <h1>See Player's Stats</h1>
+          <Button text='View Players' type='primary' onClick={onNavigate} />
+        </div>
+      </div>
     </MainLayout>
   )
 }
